e3bc29fd77bde0a548bf0fc2651a4420da41bec5
Marcos Oliveira <ziguifrido@gmail.com>
2021-02-13 05:55:03 -0400

    useContext + Protected route + Login via e-mail

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   }
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
-  export default firebase
+export default firebase
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
     ThemeProvider,
     CSSReset,
@@ -13,10 +13,42 @@ import {
     FormControl,
     FormLabel,
     Input,
+    Alert,
+    AlertIcon,
 } from '@chakra-ui/core'
+
+import Firebase from '../config/firebase'
 import DarkModeToggle from '../components/darkModeToggle'
+import { useUserContext } from '../context/UserContext'
 
 function Login() {
+    const history = useHistory()
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [errorMessage, setErrorMessage] = useState<string>('')
+
+    const { setUserInfo } = useUserContext()
+
+    
+    const loginEmail = () => {
+        setErrorMessage('')
+
+        Firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                console.log(user.user.uid)
+                setUserInfo({
+                    uid: user.user.uid,
+                    email: user.user.email,
+                    signedIn: true
+                })
+                history.push("/")
+            })
+            .catch((error) => {
+                setErrorMessage(error.message)
+            })
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <CSSReset />
@@ -32,23 +64,28 @@ function Login() {
 
             <Grid display="flex" justifyContent="center" alignItems="center" m={10}>
                 <Box justifyContent="center" alignItems="center" width={['100%', "80%", "50%", "40%"]}>
+                    {errorMessage.length > 0 && 
+                        <Alert status="error" variant="solid">
+                            <AlertIcon/>
+                            {errorMessage}
+                        </Alert>
+                    }
                     <FormControl>    
                         <FormLabel pt={10}>E-mail</FormLabel>
-                        <Input type="email" name="email"/>
+                        <Input type="email" name="email" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
                     </FormControl>
                     <FormControl>    
                         <FormLabel pt={10}>Password</FormLabel>
-                        <Input type="password" name="password" />
+                        <Input type="password" name="password" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
                     </FormControl>
                     <FormControl className="group-button">
-                        <Divider borderColor="blackAlpha.500" mt={10} mb={10} />
-                        <Button type="submit" backgroundColor="messenger.500" color="whiteAlpha.900" mr="5" leftIcon="unlock">
+                        <Divider borderColor="blackAlpha.500" mt={10}/>
+                        <Button type="submit" backgroundColor="messenger.500" color="whiteAlpha.900" mr="5" leftIcon="unlock" onClick={() => loginEmail()}>
                             Log In
                         </Button>
                         <Button backgroundColor="whatsapp.500" color="whiteAlpha.900" mr="5" leftIcon="plus-square">
                             Sign Up
                         </Button>
-                        <Button backgroundColor="gray.500" color="whiteAlpha.900" leftIcon="arrow-back"><Link to="/">Back</Link></Button>
                     </FormControl>
                 </Box>
             </Grid>
