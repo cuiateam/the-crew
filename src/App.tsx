d73532d28790768cffe537f814268ccec26da964
Marcos Oliveira <ziguifrido@gmail.com>
2021-02-13 05:55:03 -0400

    useContext + Protected route + Login via e-mail

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import './css/main.css'
+import React, { useState } from 'react';
+import Login from './pages/login'
+import NotFound from './pages/notFound'
+import Member from './pages/member'
+import MemberList from './pages/memberList'
+import { Route, Switch, BrowserRouter }from 'react-router-dom'
+import { IoMdHeart, IoLogoGithub } from 'react-icons/io'
+import { UserContext } from './context/UserContext'
+
+const App = () => {
+  
+  const [userInfo, setUserInfo] = useState({ uid: '', email: '', signedIn: false })
+  
+  return(
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      <BrowserRouter>
+          <div className="content">
+              <Switch>
+                  {userInfo.signedIn && 
+                    <>
+                      <Route path="/" exact={true} component={MemberList} />
+                      <Route path="/member/:id" component={(props) => <Member {...props}/>} />
+                    </>
+                  }
+                  <Route path={["/", "/login", "/member/:id"]} component={Login} />
+                  <Route path="*" component={NotFound} />
+              </Switch>
+          </div>
+          <div className="footer">
+              <hr />
+              Fork me on <a href="https://github.com/cuiateam/the-crew" target="blank" ><IoLogoGithub />.</a><br /> 
+              Made with <IoMdHeart /> by <a href="https://cuiateam.github.io/site/" target="blank">Cuia Team.</a>
+          </div>
+      </ BrowserRouter>
+    </UserContext.Provider>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,10 @@
-import './css/main.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Login from './pages/login'
-import NotFound from './pages/notFound'
-import Member from './pages/member'
-import MemberList from './pages/memberList'
-import { Route, Switch, BrowserRouter }from 'react-router-dom'
-import { IoMdHeart, IoLogoGithub } from 'react-icons/io'
+import App from './App'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <div className="content">
-            <Switch>
-                <Route path="/" exact={true} component={MemberList} />
-                <Route path="/login" component={Login} />
-                <Route path="/member/:id" component={(props) => <Member {...props}/>} />
-                <Route path="*" component={NotFound} />
-            </Switch>
-        </div>
-        <div className="footer">
-            Fork me on <a href="https://github.com/cuiateam/the-crew" target="blank" ><IoLogoGithub />.</a><br /> 
-            Made with <IoMdHeart color="#ff5555" /> by <a href="https://cuiateam.github.io/site/" target="blank">Cuia Team.</a>
-        </div>
-    </ BrowserRouter>
+    <App />
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
